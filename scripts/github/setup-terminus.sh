3860c6022867f076447e8010663f1a7caf36c59c
carl-alberto <cgalbert01@gmail.com>
2023-04-05 18:39:15 +0800

    updated variables

--- scripts/github/setup-terminus.sh
+++ scripts/github/setup-terminus.sh
@@ -3,17 +3,14 @@
 export PROJECT_ROOT="$(pwd)"
 . $PROJECT_ROOT/config
 
-# mkdir ~/terminus
-# cd ~/terminus
-# curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install || exit 0
-
+# mar 2 2023 - ver 3.14 release
+# https://github.com/pantheon-systems/terminus/releases
 mkdir -p ~/terminus && cd ~/terminus
-curl -L https://github.com/pantheon-systems/terminus/releases/download/3.1.0/terminus.phar --output terminus
+curl -L https://github.com/pantheon-systems/terminus/releases/download/3.1.4/terminus.phar --output terminus
 chmod +x terminus
+# this will make sure you get the latest terminus version
 ./terminus self:update
 sudo ln -s ~/terminus/terminus /usr/local/bin/terminus
-
-
 
 # /home/runner/terminus/vendor/bin/terminus self:info
 # /home/runner/terminus/vendor/bin/terminus auth:login --machine-token="$MACHINETOKEN" --email="$PANTHEONEMAIL"
@@ -24,13 +21,13 @@ sudo ln -s ~/terminus/terminus /usr/local/bin/terminus
 
 printf "[\e[0;34mNOTICE\e[0m] Terminus Setup START!!!.\n"
 
-echo "$MACHINETOKEN"
-echo $MACHINETOKEN
+echo "$PANTHEON_TERMINUS_MACHINE_TOKEN"
+echo $PANTHEON_TERMINUS_MACHINE_TOKEN
 echo PANTHEONEMAIL
 printf "[\e[0;34mNOTICE\e[0m] Terminus Setup asdfasdffds!!.\n"
 terminus self:info
 terminus art
-terminus auth:login --machine-token="$MACHINETOKEN" --email="$PANTHEONEMAIL"
+terminus auth:login --machine-token="$PANTHEON_TERMINUS_MACHINE_TOKEN"
 terminus auth:whoami
 #terminus connection:set $PANTHEONSITENAME.$PANTHEONENV sftp
 
