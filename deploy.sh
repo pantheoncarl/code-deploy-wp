a6b0d8171c4288bfa1e7b1ad96399c1a601d2204
carl-alberto <cgalbert01@gmail.com>
2023-04-05 20:07:27 +0800

    cleanup

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Note that this does not use pipefail
-# because if the grep later doesn't match any deleted files,
-# which is likely the majority case,
-# it does not exit with a 0, and I only care about the final exit.
-set -eo
-
-echo 'test'
